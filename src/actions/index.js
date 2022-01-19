4dc42e07defb61a7d2ab2e24f2a7cb27d082d153
Savannah <savmaxwell@gmail.com>
2022-01-19 16:45:56 -0700

    applied thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,21 @@
+import axios from 'axios';
+
 export const FETCH_START = "FETCH_START";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_FAIL = "FETCH_FAIL";
+
+export const getSpells = () => {
+    return (dispatch => {
+        dispatch(fetchStart())
+        axios.get('https://api.open5e.com/spells/')
+        .then(resp => {
+            dispatch(fetchSuccess(resp.data.results))
+        })
+        .catch(err => {
+            dispatch(fetchFail(err))
+        })
+    })
+}
 
 export const fetchStart = () => {
     return ({type: FETCH_START});
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import reducer from './reducers/index'
 
 import App from './App';
 import './index.css';
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk, logger))
 
 ReactDOM.render(
   <Provider store={store}>
