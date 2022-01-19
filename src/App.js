e27b5e85742ee689f96ec8a8ec4ff868991fc81d
Savannah <savmaxwell@gmail.com>
2022-01-19 13:13:50 -0700

    built dom and basic styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Async Redux Project
+      <h1>DnD Spell Grabber</h1>
+      <div className="search-bar">
+        <form>
+          <input type="text" placeholder="Search for a spell"/>
+          <button>Search</button>
+        </form>
+      </div>
+      <div className="search-results">
+        <div className="spell-container">
+          <h2>Name:</h2>
+          <div className="spell-info">
+            <p>Level</p>
+            <p>Casting Time:</p>
+            <p>Range/Area:</p>
+            <p>School:</p>
+            <p>Damage/Effect:</p>
+          </div>
+          <div className="spell-description">
+            <p>Description</p>
+            <p>Higher Levels</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
